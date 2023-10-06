f590c77fbe11b7a1770ef9f2d7deb43a422e6c0a
Yunfan Yang <yunfan.yang@agvisorpro.com>
2023-10-06 00:27:25 -0600

    Init project

---
+++ ast-test.py
@@ -0,0 +1,42 @@
+from _ast import Constant
+import ast
+from typing import Any
+
+code = """
+a = 111
+
+class A:
+    def method1(self):
+        pass
+
+class B(A):
+    def method2(self):
+        pass
+
+def function1():
+    a = A()
+    b = B()
+    b.method2()
+
+# Some comments
+"""
+
+tree = ast.parse(code)
+print(tree)
+
+class CodeAnalyzer(ast.NodeVisitor):
+    def visit_Constant(self, node: Constant) -> Any:
+        print(f"Found constant {node.value} on line {node.lineno}")
+        return super().visit_Constant(node)
+    
+    def visit_FunctionDef(self, node):
+        print(f"Function named {node.name} defined on line {node.lineno}")
+        self.generic_visit(node)
+
+    def visit_Call(self, node):
+        if isinstance(node.func, ast.Name):
+            print(f"  Calls function named {node.func.id} on line {node.lineno}")
+        self.generic_visit(node)
+
+analyzer = CodeAnalyzer()
+analyzer.visit(tree)
