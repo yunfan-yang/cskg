dfaed3984c2891ebfd5087b04157f1ca5637516e
Yunfan Yang <yunfan.yang@agvisorpro.com>
2023-10-12 22:37:16 -0600

    Update ast-test.py

--- ast-test.py
+++ ast-test.py
@@ -1,5 +1,5 @@
-from _ast import Constant
 import ast
+from ast import Constant, Call, stmt, FunctionDef
 from typing import Any
 
 code = """
@@ -24,19 +24,27 @@ def function1():
 tree = ast.parse(code)
 print(tree)
 
+
 class CodeAnalyzer(ast.NodeVisitor):
+    def __init__(self):
+        super().__init__()
+
     def visit_Constant(self, node: Constant) -> Any:
         print(f"Found constant {node.value} on line {node.lineno}")
         return super().visit_Constant(node)
-    
-    def visit_FunctionDef(self, node):
+
+    def visit_FunctionDef(self, node: FunctionDef):
         print(f"Function named {node.name} defined on line {node.lineno}")
         self.generic_visit(node)
 
-    def visit_Call(self, node):
+    def visit_stmt(self, node: stmt) -> None:
+        self.generic_visit(node)
+
+    def visit_Call(self, node: Call):
         if isinstance(node.func, ast.Name):
             print(f"  Calls function named {node.func.id} on line {node.lineno}")
         self.generic_visit(node)
+
 
 analyzer = CodeAnalyzer()
 analyzer.visit(tree)
