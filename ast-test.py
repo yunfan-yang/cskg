d554ca0ec551fdf4fca531c07513383c0f3eea0e
Yunfan Yang <yunfan.yang@agvisorpro.com>
2023-10-13 00:03:47 -0600

    Add func_params

--- ast-test.py
+++ ast-test.py
@@ -12,11 +12,20 @@ class A:
 class B(A):
     def method2(self):
         pass
+        
+    def method3(self, a, b):
+        pass
 
 def function1():
     a = A()
     b = B()
     b.method2()
+
+def function4(param1, param2 = None):
+    print(param1, param2)
+
+def example(a, b=10, *args, c, d=20, **kwargs):
+    pass
 
 # Some comments
 """
@@ -35,6 +44,7 @@ class CodeAnalyzer(ast.NodeVisitor):
 
     def visit_FunctionDef(self, node: FunctionDef):
         print(f"Function named {node.name} defined on line {node.lineno}")
+        func_params(node)
         self.generic_visit(node)
 
     def visit_stmt(self, node: stmt) -> None:
@@ -44,6 +54,37 @@ class CodeAnalyzer(ast.NodeVisitor):
         if isinstance(node.func, ast.Name):
             print(f"  Calls function named {node.func.id} on line {node.lineno}")
         self.generic_visit(node)
+
+
+def func_params(node: FunctionDef) -> list:
+    # Print positional arguments and their default values
+    for arg, default in zip(
+        node.args.args[-len(node.args.defaults) :], node.args.defaults
+    ):
+        print(f"  Argument {arg.arg} with default value {ast.dump(default)}")
+    for arg in node.args.args[
+        : -len(node.args.defaults) if node.args.defaults else None
+    ]:
+        print(f"  Argument {arg.arg} without default value")
+
+    # Print *args-style arguments
+    if node.args.vararg:
+        print(f"  *{node.args.vararg.arg} (Varargs)")
+
+    # Print keyword-only arguments and their default values
+    for kwarg, kw_default in zip(node.args.kwonlyargs, node.args.kw_defaults):
+        if kw_default:
+            print(
+                f"  Keyword-only argument {kwarg.arg} with default value {ast.dump(kw_default)}"
+            )
+        else:
+            print(f"  Keyword-only argument {kwarg.arg} without default value")
+
+    # Print **kwargs-style arguments
+    if node.args.kwarg:
+        print(f"  **{node.args.kwarg.arg} (Keyword Varargs)")
+
+    return node.args
 
 
 analyzer = CodeAnalyzer()
