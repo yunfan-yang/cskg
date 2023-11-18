472e2d12eb8cb2b6b07757466b5be716be535c78
Yunfan Yang <yunfan.yang@agvisorpro.com>
2023-11-17 20:16:34 -0700

    Fix traversal

--- code-analyzer.py
+++ code-analyzer.py
@@ -1,5 +1,6 @@
 import os
 import astroid
+import neomodel
 
 from models import Class, Function
 
@@ -8,48 +9,68 @@ class CodeAnalyzer:
     def __init__(self, folder_path):
         self.folder_path = folder_path
 
-    def traverse(self):
+    def analyze(self):
+        self.__traverse_files()
+
+    def __traverse_files(self):
         for root, dirs, files in os.walk(self.folder_path):
             for file in files:
                 if file.endswith(".py"):
                     file_path = os.path.join(root, file)
-                    self.extract_file(file_path)
+                    self.__extract_file(file_path)
 
-    def extract_file(self, file_path):
+    def __extract_file(self, file_path):
         module_name = file_path.split("/")[-1].split(".")[0]
 
         with open(file_path, "r") as file:
             code = file.read()
             tree = astroid.parse(code, module_name, file_path)
-            children = tree.get_children()
+            entities = self.__visit_children(tree)
+            for entity in entities:
+                entity.save()
 
-            for node in children:
-                if isinstance(node, astroid.ClassDef):
-                    class_name = node.name
-                    class_qualified_name = node.qname()
-                    print(f"Class: {class_name} ({class_qualified_name})")
+    def __visit_children(self, node) -> list[neomodel.StructuredNode]:
+        entities = []
 
-                    c = Class(name=class_name, qualified_name=class_qualified_name)
-                    c.save()
+        children = node.get_children()
+        for child in children:
+            es = self.__visit_node(child)
+            entities.extend(es)
 
-                    children = node.get_children()
-                    for child in children:
-                        if isinstance(child, astroid.FunctionDef):
-                            function_name = child.name
-                            function_qualified_name = child.qname()
-                            args = child.args
-                            print(f"Function: {function_name}")
+        return entities
 
-                            f = Function(
-                                name=function_name,
-                                qualified_name=function_qualified_name,
-                                args=args,
-                            )
-                            f.save()
+    def __visit_node(self, node) -> list[neomodel.StructuredNode]:
+        entities = []
 
-                            c.contains.connect(f)
-                            c.save()
+        if isinstance(node, astroid.ClassDef):
+            c = self.__visit_class(node)
+            entities.append(c)
+
+        elif isinstance(node, astroid.FunctionDef):
+            f = self.__visit_function(node)
+            entities.append(f)
+
+        return entities
+
+    def __visit_class(self, node: astroid.ClassDef) -> Class:
+        name = node.name
+        qualified_name = node.qname()
+        print(f"Class: {name} ({qualified_name})")
+
+        c = Class(name=name, qualified_name=qualified_name)
+        self.__visit_children(node)
+
+        return c
+
+    def __visit_function(self, node: astroid.FunctionDef) -> Function:
+        name = node.name
+        qualified_name = node.qname()
+        args = node.args
+        print(f"Function: {name} ({qualified_name})")
+
+        f = Function(name=name, qualified_name=qualified_name, args=args)
+        return f
 
 
 ca = CodeAnalyzer("target/requests")
-ca.traverse()
+ca.analyze()
