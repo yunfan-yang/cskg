8ad8d5fe851436925cf4ba20eaf5435ef5f8111e
Yunfan Yang <yunfan.yang@agvisorpro.com>
2023-11-17 19:22:16 -0700

    Add class functions

--- code-analyzer.py
+++ code-analyzer.py
@@ -30,6 +30,20 @@ class CodeAnalyzer:
                     c = Class(name=class_name, full_name=class_full_name)
                     c.save()
 
+                    children = node.get_children()
+                    for child in children:
+                        if isinstance(child, astroid.FunctionDef):
+                            function_name = child.name
+                            function_full_name = child.qname()
+                            args = child.args
+                            print(f"Function: {function_name} ({function_full_name})")
+
+                            f = Function(name=function_name, full_name=function_full_name, args=args)
+                            f.save()
+
+                            c.contains.connect(f)
+                            c.save()
+
 
 ca = CodeAnalyzer("target/requests")
 ca.traverse()
