31a3b7bc000ea5aaba16afd7cf338f9e18dc6851
Yunfan Yang <cloudy.young@outlook.com>
2024-01-11 20:19:43 -0700

    Add mongodb

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
+import json
 import os
 from dotenv import load_dotenv
 import neomodel
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from pymongo import MongoClient
 
 from analyzer.code_analyzer import CodeAnalyzer
-from analyzer.models.relational import *
 
 
 load_dotenv()
 
 NEO4J_URL = os.environ.get("NEO4J_URL")
 POSTGRES_URL = os.environ.get("POSTGRES_URL")
+MONGO_URL = os.environ.get("MONGO_URL")
 
 neomodel.config.DATABASE_URL = NEO4J_URL
 neomodel.config.AUTO_INSTALL_LABELS = True
@@ -19,13 +21,19 @@ neomodel.config.AUTO_INSTALL_LABELS = True
 postgres_engine = create_engine(POSTGRES_URL)
 postgres_session = Session(bind=postgres_engine)
 
+mongo_client = MongoClient(MONGO_URL)
+database = mongo_client.code_analyzer
+classes = database.classes
+functions = database.functions
+calls_rel = database.calls_rel
+inherits_rel = database.inherits_rel
 
 # Clean database
-postgres_session.query(ClassRow).delete()
-postgres_session.query(FunctionRow).delete()
-postgres_session.query(CallsRelRow).delete()
-postgres_session.query(InheritsRelRow).delete()
-postgres_session.commit()
+# postgres_session.query(ClassRow).delete()
+# postgres_session.query(FunctionRow).delete()
+# postgres_session.query(CallsRelRow).delete()
+# postgres_session.query(InheritsRelRow).delete()
+# postgres_session.commit()
 # neomodel.db.cypher_query("MATCH (n) DETACH DELETE n")
 # neomodel.db.cypher_query("DROP CONSTRAINT constraint_unique_Function_qualified_name")
 # neomodel.db.cypher_query("DROP CONSTRAINT constraint_unique_Class_qualified_name")
@@ -41,3 +49,5 @@ while True:
         print(c)
     except:
         break
+
+mongo_client.close()
