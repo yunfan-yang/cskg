52d783883daf97bd3a7c05561f5eff0b5a2afa15
Yunfan Yang <cloudy.young@outlook.com>
2024-01-12 16:57:13 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,41 +1,30 @@
-import json
 import os
 from dotenv import load_dotenv
 import neomodel
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
 from pymongo import MongoClient
 from loguru import logger
 
 from interpreter.interpreter import CodeInterpreter
 
-
+# Load environment variables
 load_dotenv()
 
 NEO4J_URL = os.environ.get("NEO4J_URL")
-POSTGRES_URL = os.environ.get("POSTGRES_URL")
 MONGO_URL = os.environ.get("MONGO_URL")
 
 neomodel.config.DATABASE_URL = NEO4J_URL
 neomodel.config.AUTO_INSTALL_LABELS = True
 
-postgres_engine = create_engine(POSTGRES_URL)
-postgres_session = Session(bind=postgres_engine)
-
+# Instantiate mongo db client
 mongo_client = MongoClient(MONGO_URL)
-database = mongo_client.code_interpreter
+mongo_db = mongo_client.code_interpreter
 
-classes = database.classes
-functions = database.functions
-calls_rel = database.calls_rel
-inherits_rel = database.inherits_rel
+# List all collections and drop all
+for collection_name in mongo_db.list_collection_names():
+    mongo_db.drop_collection(collection_name)
 
-classes.drop()
-functions.drop()
-calls_rel.drop()
-inherits_rel.drop()
 
-# Clean database
+# Clean neo database
 # neomodel.db.cypher_query("MATCH (n) DETACH DELETE n")
 # neomodel.db.cypher_query("DROP CONSTRAINT constraint_unique_Function_qualified_name")
 # neomodel.db.cypher_query("DROP CONSTRAINT constraint_unique_Class_qualified_name")
@@ -49,17 +38,10 @@ generator = ca.analyze()
 while True:
     try:
         node = next(generator)
-
         logger.info(node)
 
-        if node.get("type") == "class":
-            classes.insert_one(node)
-        elif node.get("type") == "function":
-            functions.insert_one(node)
-        elif node.get("type") == "calls_rel":
-            calls_rel.insert_one(node)
-        elif node.get("type") == "inherits_rel":
-            inherits_rel.insert_one(node)
+        node_type = node.get("type")
+        mongo_db[node_type].insert_one(node)
     except:
         break
 
