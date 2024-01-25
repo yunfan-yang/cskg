37af2246dbeed53fc436157dfea328afcc966fbc
Yunfan Yang <cloudy.young@outlook.com>
2024-01-24 18:04:31 -0700

    Update interpreter.py

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -21,19 +21,8 @@ class CodeInterpreter:
             for file in files:
                 if file.endswith(".py"):  # Only handles python file
                     current_file_path = os.path.join(root, file)
-
                     ast = self.manager.ast_from_file(current_file_path)
                     asts[current_file_path] = ast
 
         for file_path, ast in asts.items():
             yield from visit_children(ast, file_path)
-
-    def extract_file(self, current_file_path: str = None):
-        logger.debug(f"extract file {current_file_path}")
-        module_name = current_file_path.split("/")[-1].split(".")[0]
-
-        with open(current_file_path, "r") as file:
-            code = file.read()
-            tree = astroid.parse(code, module_name, current_file_path)
-
-            yield from visit_children(tree, current_file_path)
