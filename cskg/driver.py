efac12bd4ada544d9d035857be52440e76c4514c
Yunfan Yang <cloudy.young@outlook.com>
2024-03-06 17:21:33 -0700

    Update driver.py

--- cskg/driver.py
+++ cskg/driver.py
@@ -13,10 +13,13 @@ from cskg.composer.node_composers import EntityComposer, RelationshipComposer
 # Initialize logger file
 filename = time.strftime("%Y-%m-%d_%H-%M-%S")
 logger.add(
-    f"logs/{filename}.log", filter=lambda record: record["level"].name != "DEBUG"
+    f"logs/{filename}.log", filter=lambda record: record["level"].name not in ["DEBUG", "ERROR"]
 )
 logger.add(
     f"logs/{filename}.debug.log", filter=lambda record: record["level"].name == "DEBUG"
+)
+logger.add(
+    f"logs/{filename}.error.log", filter=lambda record: record["level"].name == "ERROR"
 )
 
 
