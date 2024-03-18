58b870abc75dc94b0acbe21ab1829d3541e7e430
Yunfan Yang <cloudy.young@outlook.com>
2024-03-18 17:23:44 -0600

    Create constants.py

---
+++ cskg/constants.py
@@ -0,0 +1,19 @@
+from enum import StrEnum
+
+
+class Entity(StrEnum):
+    MODULE = "module"
+    CLASS = "class"
+    VARIABLE = "variable"
+    FUNCTION = "function"
+    METHOD = "method"
+
+
+class Relationship(StrEnum):
+    CALLS = "CALLS"
+    INHERITS = "INHERITS"
+    CONTAINS = "CONTAINS"
+    TAKES = "TAKES"
+    RETURNS = "RETURNS"
+    INSTANTIATES = "INSTANTIATES"
+    USES = "USES"
