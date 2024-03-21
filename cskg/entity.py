9f1aae412ad8e6d8c74341952c0e9e807eb18aef
Yunfan Yang <cloudy.young@outlook.com>
2024-03-21 05:23:31 -0600

    Update entity.py

--- cskg/entity.py
+++ cskg/entity.py
@@ -1,18 +1,29 @@
-class Entity(dict):
+from typing import Any
+from abc import ABC, ABCMeta
+
+
+class EntityMeta(ABCMeta):
+    def __init__(cls, name, bases, dct):
+        if not hasattr(cls, "type") or not hasattr(cls, "label"):
+            raise AttributeError(f"Class {name} lacks required 'label' class attribute")
+        super().__init__(name, bases, dct)
+
+
+class Entity(dict, ABC, metaclass=EntityMeta):
     __final_fields__ = ["type", "label"]
+    type = "entity"
+    label = "Entity"
 
     def __init__(
         self,
-        type: str,
-        label: str | set[str],
         name: str,
         qualified_name: str,
         file_path: str,
         **kwargs,
     ):
         super().__init__(
-            type=type,
-            label=label,
+            type=self.type,
+            label=self.label,
             name=name,
             qualified_name=qualified_name,
             file_path=file_path,
@@ -21,7 +32,7 @@ class Entity(dict):
 
     def __setitem__(self, key, value):
         if key in self.__final_fields__:
-            raise ValueError("Cannot set type")
+            raise ValueError(f"Cannot set {key}")
         super().__setitem__(key, value)
 
     def __getitem__(self, key):
@@ -29,99 +40,38 @@ class Entity(dict):
             return self.get(key)
         return super().__getitem__(key)
 
-    # def __repr__(self):
-    # return f"<Entity {self.get('type')} {self.get("qualified_name")}>"
+    def __getattribute__(self, __name: str) -> Any:
+        try:
+            return super().__getitem__(__name)
+        except KeyError:
+            return super().__getattribute__(__name)
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name in self.__final_fields__:
+            raise ValueError(f"Cannot set {__name}")
+        super().__setitem__(__name, __value)
 
 
 class ModuleEntity(Entity):
-    def __init__(self, name: str, qualified_name: str, file_path: str, **kwargs):
-        super().__init__(
-            type="module_ent",
-            label="Module",
-            name=name,
-            qualified_name=qualified_name,
-            file_path=file_path,
-            **kwargs,
-        )
+    type = "module_ent"
+    label = "Module"
 
 
 class ClassEntity(Entity):
-    def __init__(
-        self,
-        name: str,
-        qualified_name: str,
-        file_path: str,
-        **kwargs,
-    ):
-        super().__init__(
-            type="class_ent",
-            label="Class",
-            name=name,
-            qualified_name=qualified_name,
-            file_path=file_path,
-            **kwargs,
-        )
+    type = "class_ent"
+    label = "Class"
 
 
 class FunctionEntity(Entity):
-    def __init__(
-        self,
-        name: str,
-        qualified_name: str,
-        file_path: str,
-        subtype: str,
-        **kwargs,
-    ):
-        super().__init__(
-            type="function_ent",
-            label="Function",
-            name=name,
-            qualified_name=qualified_name,
-            file_path=file_path,
-            subtype=subtype,
-            **kwargs,
-        )
+    type = "function_ent"
+    label = "Function"
 
 
 class MethodEntity(FunctionEntity):
-    def __init__(
-        self,
-        name: str,
-        qualified_name: str,
-        file_path: str,
-        class_name: str,
-        class_qualified_name: str,
-        subtype: str,
-        **kwargs,
-    ):
-        super(Entity, self).__init__(
-            type="method_ent",
-            label=("Method", "Function"),
-            name=name,
-            qualified_name=qualified_name,
-            file_path=file_path,
-            class_name=class_name,
-            class_qualified_name=class_qualified_name,
-            subtype=subtype,
-            **kwargs,
-        )
+    type = "method_ent"
+    label = ("Method", "Function")
 
 
 class VariableEntity(Entity):
-    def __init__(
-        self,
-        name: str,
-        qualified_name: str,
-        file_path: str,
-        access: str,
-        **kwargs,
-    ):
-        super().__init__(
-            type="variable_ent",
-            label="Variable",
-            name=name,
-            qualified_name=qualified_name,
-            file_path=file_path,
-            access=access,
-            **kwargs,
-        )
+    type = "variable_ent"
+    label = "Variable"
