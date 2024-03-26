51ef14e458f03976c9f2cad67e6aadcf18b7896a
Yunfan Yang <cloudy.young@outlook.com>
2024-03-25 20:55:01 -0600

    Fix module name

--- cskg/interpreter/module.py
+++ cskg/interpreter/module.py
@@ -6,7 +6,7 @@ from cskg.interpreter.vars import visit_local_variables
 
 
 def visit_module(module: Module):
-    name = module.name
+    name = get_module_name(module)
     qualified_name = module.qname()
     file_path = module.root().file
 
@@ -19,3 +19,7 @@ def visit_module(module: Module):
 
     yield from visit_local_variables(module)
     yield from visit_children(module)
+
+
+def get_module_name(module: Module):
+    return module.qname().split(".").pop(-1)
