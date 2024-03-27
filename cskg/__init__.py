90cdb584b76209ebf7ceb9b979ab9717f840b281
Yunfan Yang <cloudy.young@outlook.com>
2024-03-27 02:55:40 -0600

    Import all modules dynamically

--- cskg/__init__.py
+++ cskg/__init__.py
@@ -1,4 +1,7 @@
 import time
+import os
+import sys
+import importlib
 from loguru import logger
 
 # Initialize logger file
@@ -13,3 +16,20 @@ logger.add(
 logger.add(
     f"logs/{filename}.error.log", filter=lambda record: record["level"].name == "ERROR"
 )
+
+
+# Import all modules
+def import_classes_from_directory(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith(".py") and not filename.startswith("__"):
+            module_name = filename[:-3]  # Remove .py extension
+            module_path = os.path.join(directory, filename)
+
+            # Create module spec and load the module
+            spec = importlib.util.spec_from_file_location(module_name, module_path)
+            module = importlib.util.module_from_spec(spec)
+            sys.modules[module_name] = module
+            spec.loader.exec_module(module)
+
+
+import_classes_from_directory("cskg/detectors")
