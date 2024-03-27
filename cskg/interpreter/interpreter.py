6795268790d2bfb0a348933254ec054b8fdc1e97
Yunfan Yang <cloudy.young@outlook.com>
2024-03-27 03:02:33 -0600

    Update interpreter.py

--- cskg/interpreter/interpreter.py
+++ cskg/interpreter/interpreter.py
@@ -9,9 +9,9 @@ from cskg.interpreter.nodes import visit_node
 
 class CodeInterpreter:
     def __init__(self, folder_path):
-        os.chdir(folder_path)
         self.folder_path = folder_path
-        self.folder_full_path = os.getcwd()
+        self.folder_abs_path = os.path.abspath(folder_path)
+        logger.debug(f"Interpreting code in folder: {self.folder_abs_path}")
         self.manager = AstroidManager()
         self.manager.register_transform(Module, self.format_qname)
         self.manager.register_transform(ClassDef, self.format_qname)
@@ -23,7 +23,7 @@ class CodeInterpreter:
     def traverse_files(self):
         asts = {}
 
-        for root, dirs, files in os.walk(self.folder_full_path):
+        for root, dirs, files in os.walk(self.folder_abs_path):
             for file in files:
                 if file.endswith(".py"):  # Only handles python file
                     file_path = os.path.join(root, file)
@@ -42,10 +42,10 @@ class CodeInterpreter:
 
         # Set file path to None if it is not internal entity
         node_root_file = node.root().file
-        if node_root_file and not node_root_file.startswith(self.folder_full_path):
+        if node_root_file and not node_root_file.startswith(self.folder_abs_path):
             node.root().file = None
 
         return node
 
     def get_module_prefix(self):
-        return self.folder_full_path.split("/")[-1]
+        return self.folder_abs_path.split("/")[-1]
