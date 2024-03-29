fe53307e1e3d605c603331476f7022a69bc67b1f
Yunfan Yang <cloudy.young@outlook.com>
2024-03-29 07:53:29 -0600

    Update docker

--- test.py
+++ test.py
@@ -6,14 +6,9 @@ from cskg.driver import Driver
 # Load environment variables
 load_dotenv()
 
-NEO4J_URL = os.environ.get("NEO4J_URL")
-MONGO_URL = os.environ.get("MONGO_URL")
 
 NEO4J_REQUESTS_URL = os.environ.get("NEO4J_REQUESTS_URL")
 MONGO_REQUESTS_URL = os.environ.get("MONGO_REQUESTS_URL")
-
-driver = Driver("targets/transformers", neo4j_url=NEO4J_URL, mongo_url=MONGO_URL)
-driver.run(interpret=False, compose=False, detect=True)
 
 driver_requests = Driver(
     "targets/requests",
@@ -21,3 +16,13 @@ driver_requests = Driver(
     mongo_url=MONGO_REQUESTS_URL,
 )
 driver_requests.run(interpret=True, compose=True, detect=True)
+
+
+NEO4J_URL_TRANSFORMERS = os.environ.get("NEO4J_URL_TRANSFORMERS")
+MONGO_URL_TRANSFORMERS = os.environ.get("MONGO_URL_TRANSFORMERS")
+driver_transformers = Driver(
+    "targets/transformers",
+    neo4j_url=NEO4J_URL_TRANSFORMERS,
+    mongo_url=MONGO_URL_TRANSFORMERS,
+)
+driver_transformers.run(interpret=True, compose=True, detect=True)
