2c3f6871b78d8fa97ae243c6c79bc2eeb63ffa83
Yunfan Yang <cloudy.young@outlook.com>
2024-03-29 05:26:58 -0600

    Update test.py

--- test.py
+++ test.py
@@ -9,5 +9,5 @@ load_dotenv()
 NEO4J_URL = os.environ.get("NEO4J_URL")
 MONGO_URL = os.environ.get("MONGO_URL")
 
-driver = Driver("targets/requests", neo4j_url=NEO4J_URL, mongo_url=MONGO_URL)
-driver.run()
+driver = Driver("targets/transformers", neo4j_url=NEO4J_URL, mongo_url=MONGO_URL)
+driver.run(interpret=False, compose=True, detect=True)
