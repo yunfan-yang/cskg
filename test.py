586d653e2291dad40b7a329007dae7412e1ada85
Yunfan Yang <cloudy.young@outlook.com>
2024-04-08 10:33:41 -0600

    Update test.py

--- test.py
+++ test.py
@@ -7,22 +7,22 @@ from cskg.driver import Driver
 load_dotenv()
 
 
-NEO4J_REQUESTS_URL = os.environ.get("NEO4J_REQUESTS_URL")
-MONGO_REQUESTS_URL = os.environ.get("MONGO_REQUESTS_URL")
+# NEO4J_REQUESTS_URL = os.environ.get("NEO4J_REQUESTS_URL")
+# MONGO_REQUESTS_URL = os.environ.get("MONGO_REQUESTS_URL")
 
-driver_requests = Driver(
-    "targets/requests",
-    neo4j_url=NEO4J_REQUESTS_URL,
-    mongo_url=MONGO_REQUESTS_URL,
-)
-driver_requests.run(interpret=False, compose=False, detect=True)
-
-
-# NEO4J_URL_TRANSFORMERS = os.environ.get("NEO4J_URL_TRANSFORMERS")
-# MONGO_URL_TRANSFORMERS = os.environ.get("MONGO_URL_TRANSFORMERS")
-# driver_transformers = Driver(
-#     "targets/transformers",
-#     neo4j_url=NEO4J_URL_TRANSFORMERS,
-#     mongo_url=MONGO_URL_TRANSFORMERS,
+# driver_requests = Driver(
+#     "targets/requests",
+#     neo4j_url=NEO4J_REQUESTS_URL,
+#     mongo_url=MONGO_REQUESTS_URL,
 # )
-# driver_transformers.run(interpret=False, compose=False, detect=True)
+# driver_requests.run(interpret=False, compose=False, detect=True)
+
+
+NEO4J_URL_TRANSFORMERS = os.environ.get("NEO4J_URL_TRANSFORMERS")
+MONGO_URL_TRANSFORMERS = os.environ.get("MONGO_URL_TRANSFORMERS")
+driver_transformers = Driver(
+    "targets/transformers",
+    neo4j_url=NEO4J_URL_TRANSFORMERS,
+    mongo_url=MONGO_URL_TRANSFORMERS,
+)
+driver_transformers.run(interpret=True, compose=True, detect=True)
